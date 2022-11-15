8d5448536f1fcc2c9aab1f57808bc9dc07f8b586
lin-1214 <b10901059@g.ntu.edu.tw>
2022-11-15 19:28:45 +0800

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
 import logo from "./logo.svg";
 import "./App.css";
+import MainPage from "./pages/mainPage";
+import { Hidden } from "@material-ui/core";
 
 function App() {
-  return <div className="App"></div>;
-=======
-import logo from './logo.svg';
-import './App.css';
-import MainPage from './pages/mainPage';
-import { Hidden } from '@material-ui/core';
-
-function App() {
-  return (
-    <MainPage></MainPage>
-  );
->>>>>>> f5935e23e6ff9130171bf3e85b123d4bf015775e
+  return <MainPage></MainPage>;
 }
 
 export default App;
