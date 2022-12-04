ea25ed1318dc62d72908d102595d192b92f53d35
xNTsai <johnnytsai920315@gmail.com>
2022-12-05 01:13:02 +0800

    Calendar_differentViews

--- frontend/src/components/calendar.js
+++ frontend/src/components/calendar.js
@@ -3,34 +3,32 @@ import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
 import bootstrap5Plugin from '@fullcalendar/bootstrap5'
+import listPlugin from '@fullcalendar/list';
+import timeGridPlugin from '@fullcalendar/timegrid';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'; // needs additional webpack config!
+import { unstable_detectScrollType } from '@mui/utils'
 
 const MyCalendar = () => {
   const [events, setEvents] = useState([])
-  const [themes, setThemes] = useState([])
-
-  useEffect(()=>{
-    fetch('https://bootswatch.com/api/5.json')
-  .then(response => response.json()).then(result => setThemes(result.themes));
-  }, [])  // get 
 
   const handleDateClick = (arg) => { // bind with an arrow function
     console.log(arg)
     let event = prompt("Create an event from "+arg.startStr+" to "+arg.endStr+"(excl)\nEnter the event :")
-    console.log(!event)
     if(event)setEvents([...events, {title: event, start: arg.startStr, end: arg.endStr}])
   }
 
   return(
     <FullCalendar
-        plugins={[ dayGridPlugin, interactionPlugin, bootstrap5Plugin]}
+        plugins={[ dayGridPlugin, interactionPlugin, bootstrap5Plugin, timeGridPlugin, listPlugin, dayGridPlugin]}
         themeSystem= 'bootstrap5'
         initialView="dayGridMonth"
         selectable={true}
         selectMirror={true}
         select={(e) => handleDateClick(e)}
         events={events}
+        headerToolbar={{left: 'today prev,next', center: "title", end: 'dayGridMonth timeGridWeek timeGridDay listWeek'}}
+        // initialView='timeGridWeek'
         
       />
   )
