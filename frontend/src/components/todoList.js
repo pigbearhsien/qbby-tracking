0227f82059e7970939eedc00e2b74121d0c18cf3
lin-1214 <b10901059@g.ntu.edu.tw>
2022-12-05 11:41:31 +0800

    todolist

--- frontend/src/components/todoList.js
+++ frontend/src/components/todoList.js
@@ -1,9 +1,33 @@
-import { react, useState } from "react";
+import { react, useEffect, useState } from "react";
 import { FcEngineering } from "react-icons/fc";
+import { IconButton } from "@mui/material";
+import { GrFormAdd } from "react-icons/gr";
+import TextField from "@mui/material/TextField";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import FormHelperText from "@mui/material/FormHelperText";
 import "./todoList.css";
 
-const todoList = () => {
+const TodoList = () => {
+  const [newTodo, setNewTodo] = useState("");
   const [todos, setTodos] = useState([]);
+  const [load, setLoad] = useState(true);
+
+  const todoFormat = () => {
+    return (
+      <>
+        {todos.map((todo) => (
+          <div className="todoContainerRow">{todo}</div>
+        ))}
+      </>
+    );
+  };
+
+  useEffect(() => {
+    //console.log(todos);
+  }, [todos]);
 
   return (
     <div
@@ -17,23 +41,67 @@ const todoList = () => {
       </div>
       <div className="todoBody">
         <div className="todoContainer">
-          <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>6</li>
-            <li>7</li>
-            <li>8</li>
-            <li>9</li>
-            <li>10</li>
-          </ul>
+          <div className="todoAddBar">
+            {load ? (
+              <FormControl variant="standard">
+                {/*<InputLabel></InputLabel>*/}
+                <Input
+                  id="input-with-icon-adornment"
+                  value={newTodo}
+                  onChange={(e) => {
+                    setNewTodo(e.target.value);
+                  }}
+                  startAdornment={
+                    <InputAdornment position="start">
+                      <GrFormAdd
+                        onClick={() => {
+                          if (!newTodo) setLoad(false);
+                          else {
+                            setTodos([...todos, newTodo]);
+                            setNewTodo("");
+                            setLoad(true);
+                          }
+                        }}
+                      ></GrFormAdd>
+                    </InputAdornment>
+                  }
+                />
+              </FormControl>
+            ) : (
+              <FormControl error variant="standard">
+                {/*<InputLabel></InputLabel>*/}
+                <Input
+                  id="input-with-icon-adornment"
+                  value={newTodo}
+                  onChange={(e) => {
+                    setNewTodo(e.target.value);
+                  }}
+                  startAdornment={
+                    <InputAdornment position="start">
+                      <GrFormAdd
+                        onClick={() => {
+                          if (!newTodo) setLoad(false);
+                          else {
+                            setTodos([...todos, newTodo]);
+                            setNewTodo("");
+                            setLoad(true);
+                          }
+                        }}
+                      ></GrFormAdd>
+                    </InputAdornment>
+                  }
+                />
+                <FormHelperText id="component-error-text">
+                  尚未輸入代辦事項
+                </FormHelperText>
+              </FormControl>
+            )}
+          </div>
+          {todoFormat()}
         </div>
       </div>
-      <div className="todoFooter"></div>
     </div>
   );
 };
 
-export default todoList;
+export default TodoList;
