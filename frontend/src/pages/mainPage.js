2fe459f15daa9ed658740a54c738cde5dad3e875
lin-1214 <b10901059@g.ntu.edu.tw>
2022-12-13 23:35:09 +0800

    fix conflict

--- frontend/src/pages/mainPage.js
+++ frontend/src/pages/mainPage.js
@@ -4,13 +4,13 @@ import MoneyBar from "../components/moneyBar";
 import Profile from "../components/profile";
 import TodoList from "../components/todoList";
 import "./mainPage.css";
-import "./popUp.css"
+import "./popUp.css";
 import Popup from "reactjs-popup";
 import bgImg from "../assets/Background70.png";
 import useSelection from "antd/es/table/hooks/useSelection";
 import { useState } from "react";
-import savemoney from "../images/savemoney.gif"
-import banners from "../images/banners.gif"
+import savemoney from "../images/savemoney.gif";
+import banners from "../images/banners.gif";
 import axios from "axios";
 
 const instance = axios.create({
@@ -18,6 +18,8 @@ const instance = axios.create({
 });
 
 const MainPage = ({ setPage }) => {
+  const [popup, setPopup] = useState(true);
+
   const storeTest = async () => {
     await instance.post("createLoginInfo/", {
       username: "Vincent",
@@ -27,9 +29,6 @@ const MainPage = ({ setPage }) => {
       money: 1000,
     });
   };
-
-const MainPage = ({ setPage }) => {
-  const [popup, setPopup] = useState(true)
 
   return (
     <div
@@ -69,27 +68,35 @@ const MainPage = ({ setPage }) => {
         }}
         closeOnDocumentClick={false}
       >
-        <div style={{backgroundColor: "white", height: "60vh", width: "30vw", borderRadius: "2vw"}}>
-          <header style={{display: "flex", justifyContent: "center"}}>
+        <div
+          style={{
+            backgroundColor: "white",
+            height: "60vh",
+            width: "30vw",
+            borderRadius: "2vw",
+          }}
+        >
+          <header style={{ display: "flex", justifyContent: "center" }}>
             <p className="popupTitle">WelCome Back !</p>
           </header>
-          <header style={{display: "flex", justifyContent: "center"}}>
-            <img src={banners} style={{width: "8vw"}}></img>
-            <div style={{width: "18vw"}}>
+          <header style={{ display: "flex", justifyContent: "center" }}>
+            <img src={banners} style={{ width: "8vw" }}></img>
+            <div style={{ width: "18vw" }}>
               <p className="popupsmallTitle">Congratulation!</p>
-              <p className="popupwords">You worked for 10 hours yesterday
-              </p>
+              <p className="popupwords">You worked for 10 hours yesterday</p>
             </div>
           </header>
-          <header style={{display: "flex", justifyContent: "center"}}>
-            <img src={savemoney} style={{width: "8vw", height: "14vh"}}></img>
-            <div style={{width: "18vw"}}>
+          <header style={{ display: "flex", justifyContent: "center" }}>
+            <img src={savemoney} style={{ width: "8vw", height: "14vh" }}></img>
+            <div style={{ width: "18vw" }}>
               <p className="popupsmallTitle">Money & Exp</p>
               <p className="popupwords">You won 1000 $ and 10000 exp !</p>
             </div>
           </header>
-          <header style={{display: "flex", justifyContent: "center"}}>
-            <button className="popupbtn" onClick={()=>setPopup(false)}>Confirm</button>
+          <header style={{ display: "flex", justifyContent: "center" }}>
+            <button className="popupbtn" onClick={() => setPopup(false)}>
+              Confirm
+            </button>
           </header>
         </div>
       </Popup>
