1a640535c406730a562a50b6e8daad08d7571bc9
xNTsai <johnnytsai920315@gmail.com>
2022-12-30 15:03:15 +0800

    add baby profile

--- frontend/src/components/profile.js
+++ frontend/src/components/profile.js
@@ -1,10 +1,11 @@
 import { UserOutlined } from "@ant-design/icons";
 import { Avatar } from "antd";
 import React from "react";
+import baby from "../assets/profileBaby.gif"
 const Profile = () => (
   <>
     <div>
-      <Avatar shape="square" size={100} icon={<UserOutlined />} />
+      <img src={baby} style={{width: "8vw", borderRadius: "3vw", padding: "0.2vw", backgroundColor: "black", opacity: "0.9"}}></img>
     </div>
   </>
 );
