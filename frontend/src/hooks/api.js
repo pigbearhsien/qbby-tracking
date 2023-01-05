a76a8073efd1c0dc0fedffe4dd0b9044155f3297
lin-1214 <b10901059@g.ntu.edu.tw>
2023-01-05 16:51:29 +0800

    first deploy

--- frontend/src/hooks/api.js
+++ frontend/src/hooks/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const API_ROOT =
+  process.env.NODE_ENV === "production" ? "/api" : "http://localhost:4000/api";
+
 const instance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: API_ROOT,
 });
 
 export default instance;
