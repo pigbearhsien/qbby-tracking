a99a93c5a03126bf2dcf3218eb3edf147518f37a
lin-1214 <b10901059@g.ntu.edu.tw>
2023-01-05 19:24:53 +0800

    eighth deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,9 +13,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend", "build")));
 }
 
-if (process.env.NODE_ENV === "development") {
-  app.use(cors());
-}
+app.use(cors());
 
 app.use(express.json());
 app.use(function (req, res, next) {
